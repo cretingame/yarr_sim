cc858f54f28c857a49fec5261dc18cf5982389ed
Arnaud <arnaud.sautaux@gmail.com>
2016-12-05 16:31:55 -0800

    first commit

---
+++ l2_dma/l2p_dma_bench.vhd
@@ -0,0 +1,297 @@
+library IEEE;
+USE IEEE.STD_LOGIC_1164.all;
+USE IEEE.NUMERIC_STD.all;
+use IEEE.std_logic_unsigned.all; 
+
+
+entity l2p_dma_bench is
+    generic (
+		constant period : time := 100 ns;
+		constant axis_data_width_c : integer := 64;
+
+		constant wb_address_width_c : integer := 12;
+		constant wb_data_width_c : integer := 32
+	);
+	--port ();
+end l2p_dma_bench;
+
+architecture Behavioral of l2p_dma_bench is
+		signal clk_tbs : STD_LOGIC;
+		signal rst_tbs : STD_LOGIC;
+		signal rst_n_tbs : STD_LOGIC;
+		-- Test bench specific signals
+		signal step : integer range 1 to 10;
+		
+		-- From the DMA controller
+		signal dma_ctrl_target_addr_tbs : std_logic_vector(31 downto 0);
+		signal dma_ctrl_host_addr_h_tbs : std_logic_vector(31 downto 0);
+		signal dma_ctrl_host_addr_l_tbs : std_logic_vector(31 downto 0);
+		signal dma_ctrl_len_tbs         : std_logic_vector(31 downto 0);
+		signal dma_ctrl_start_l2p_tbs   : std_logic;
+		signal dma_ctrl_done_s        : std_logic;
+		signal dma_ctrl_error_s       : std_logic;
+		signal dma_ctrl_byte_swap_tbs   : std_logic_vector(1 downto 0);
+		signal dma_ctrl_abort_tbs       : std_logic;
+
+		-- To the arbiter (L2P data)
+		signal ldm_arb_tvalid_s  : std_logic;
+		signal ldm_arb_tlast_s : std_logic;
+		signal ldm_arb_tdata_s   : std_logic_vector(31 downto 0);
+		signal ldm_arb_req_s    : std_logic;
+		signal arb_ldm_gnt_tbs    : std_logic;
+
+
+		-- L2P channel control
+		signal l2p_edb_s  : std_logic;                    -- Asserted when transfer is aborted
+		signal l_wr_rdy_tbs : std_logic; -- Asserted when GN4124 is ready to receive master write
+		signal l2p_rdy_tbs  : std_logic;                    -- De-asserted to pause transdert already in progress
+		signal tx_error_tbs : std_logic;                    -- Asserted when unexpected or malformed paket received
+
+		-- DMA Interface (Pipelined Wishbone)
+		signal l2p_dma_adr_s   : std_logic_vector(31 downto 0);
+		signal l2p_dma_dat_s2m_s   : std_logic_vector(31 downto 0);
+		signal l2p_dma_dat_m2s_s   : std_logic_vector(31 downto 0);
+		signal l2p_dma_sel_s   : std_logic_vector(3 downto 0);
+		signal l2p_dma_cyc_s   : std_logic;
+		signal l2p_dma_stb_s   : std_logic;
+		signal l2p_dma_we_s    : std_logic;
+		signal l2p_dma_ack_s   : std_logic;
+		signal l2p_dma_stall_tbs : std_logic;
+		signal p2l_dma_cyc_tbs   : std_logic; -- P2L dma WB cycle for bus arbitration
+		
+		component l2p_dma_master is
+		generic (
+			g_BYTE_SWAP : boolean := false
+		);
+		port (
+			-- GN4124 core clk and reset
+			clk_i   : in std_logic;
+			rst_n_i : in std_logic;
+
+			-- From the DMA controller
+			dma_ctrl_target_addr_i : in  std_logic_vector(31 downto 0);
+			dma_ctrl_host_addr_h_i : in  std_logic_vector(31 downto 0);
+			dma_ctrl_host_addr_l_i : in  std_logic_vector(31 downto 0);
+			dma_ctrl_len_i         : in  std_logic_vector(31 downto 0);
+			dma_ctrl_start_l2p_i   : in  std_logic;
+			dma_ctrl_done_o        : out std_logic;
+			dma_ctrl_error_o       : out std_logic;
+			dma_ctrl_byte_swap_i   : in  std_logic_vector(1 downto 0);
+			dma_ctrl_abort_i       : in  std_logic;
+
+			-- To the arbiter (L2P data)
+			ldm_arb_tvalid_o  : out std_logic;
+			ldm_arb_tlast_o : out std_logic;
+			ldm_arb_tdata_o   : out std_logic_vector(31 downto 0);
+			ldm_arb_req_o    : out std_logic;
+			arb_ldm_gnt_i    : in  std_logic;
+
+
+			-- L2P channel control
+			l2p_edb_o  : out std_logic;                    -- Asserted when transfer is aborted
+			l_wr_rdy_i : in  std_logic;                    -- Asserted when GN4124 is ready to receive master write
+			l2p_rdy_i  : in  std_logic;                    -- De-asserted to pause transdert already in progress
+			tx_error_i : in  std_logic;                    -- Asserted when unexpected or malformed paket received
+
+			-- DMA Interface (Pipelined Wishbone)
+			l2p_dma_clk_i   : in  std_logic;
+			l2p_dma_adr_o   : out std_logic_vector(31 downto 0);
+			l2p_dma_dat_i   : in  std_logic_vector(31 downto 0);
+			l2p_dma_dat_o   : out std_logic_vector(31 downto 0);
+			l2p_dma_sel_o   : out std_logic_vector(3 downto 0);
+			l2p_dma_cyc_o   : out std_logic;
+			l2p_dma_stb_o   : out std_logic;
+			l2p_dma_we_o    : out std_logic;
+			l2p_dma_ack_i   : in  std_logic;
+			l2p_dma_stall_i : in  std_logic;
+			p2l_dma_cyc_i   : in  std_logic -- P2L dma WB cycle for bus arbitration
+		);
+	end component;
+		
+	component bram_wbs is
+	generic (
+		constant ADDR_WIDTH : integer := 16;
+		constant DATA_WIDTH : integer := 32 
+	);
+	port (
+		-- SYS CON
+		clk			: in std_logic;
+		rst			: in std_logic;
+		
+		-- Wishbone Slave in
+		wb_adr_i			: in std_logic_vector(wb_address_width_c-1 downto 0);
+		wb_dat_i			: in std_logic_vector(wb_data_width_c-1 downto 0);
+		wb_we_i			: in std_logic;
+		wb_stb_i			: in std_logic;
+		wb_cyc_i			: in std_logic; 
+		wb_lock_i		: in std_logic; -- nyi
+		
+		-- Wishbone Slave out
+		wb_dat_o			: out std_logic_vector(wb_data_width_c-1 downto 0);
+		wb_ack_o			: out std_logic		
+	);
+	end component;
+begin
+
+	clk_p: process
+	begin
+		clk_tbs <= '1';
+		wait for period/2;
+		clk_tbs <= '0';
+		wait for period/2;
+	end process clk_p;
+	
+	reset_p: process
+	begin
+	   rst_tbs <= '1';
+	   rst_n_tbs <= '0';
+	   wait for period;
+	   rst_tbs <= '0';
+	   rst_n_tbs <= '1';
+	   wait;
+	end process reset_p;
+	
+	stimuli_p: process
+	begin
+		step <= 1;
+		l_wr_rdy_tbs <= '1'; -- Asserted when GN4124 is ready to receive master write
+		l2p_rdy_tbs  <= '1';                    -- De-asserted to pause transdert already in progress
+		tx_error_tbs <= '0';                    -- Asserted when unexpected or malformed paket received
+		dma_ctrl_target_addr_tbs <= X"00000000";
+		dma_ctrl_host_addr_h_tbs <= X"00000000";
+		dma_ctrl_host_addr_l_tbs <= X"00000000";
+		dma_ctrl_len_tbs         <= X"00000000";
+		dma_ctrl_start_l2p_tbs   <= '0';
+		dma_ctrl_byte_swap_tbs   <= "00";
+		dma_ctrl_abort_tbs       <= '0';
+		arb_ldm_gnt_tbs    <= '1';
+		l2p_dma_stall_tbs <= '0';
+		p2l_dma_cyc_tbs   <= '0'; -- P2L dma WB cycle for bus arbitration
+		
+		wait for period;
+		
+		wait for period;
+		step <= 2;
+		l_wr_rdy_tbs <= '1'; -- Asserted when GN4124 is ready to receive master write
+		l2p_rdy_tbs  <= '1';                    -- De-asserted to pause transdert already in progress
+		tx_error_tbs <= '0';                    -- Asserted when unexpected or malformed paket received
+		dma_ctrl_target_addr_tbs <= X"00000010";
+		dma_ctrl_host_addr_h_tbs <= X"00000000";
+		dma_ctrl_host_addr_l_tbs <= X"0000005A";
+		dma_ctrl_len_tbs         <= X"00000010";
+		dma_ctrl_start_l2p_tbs   <= '1';
+		dma_ctrl_byte_swap_tbs   <= "00";
+		dma_ctrl_abort_tbs       <= '0';
+		arb_ldm_gnt_tbs    <= '1';
+		l2p_dma_stall_tbs <= '0';
+		p2l_dma_cyc_tbs   <= '0'; -- P2L dma WB cycle for bus arbitration
+	
+		wait for period;
+		step <= 3;
+		l_wr_rdy_tbs <= '1'; -- Asserted when GN4124 is ready to receive master write
+		l2p_rdy_tbs  <= '1';                    -- De-asserted to pause transdert already in progress
+		tx_error_tbs <= '0';                    -- Asserted when unexpected or malformed paket received
+		dma_ctrl_target_addr_tbs <= X"00000010";
+		dma_ctrl_host_addr_h_tbs <= X"00000000";
+		dma_ctrl_host_addr_l_tbs <= X"0000005A";
+		dma_ctrl_len_tbs         <= X"00000010";
+		dma_ctrl_start_l2p_tbs   <= '0';
+		dma_ctrl_byte_swap_tbs   <= "00";
+		dma_ctrl_abort_tbs       <= '0';
+		arb_ldm_gnt_tbs    <= '1';
+		l2p_dma_stall_tbs <= '0';
+		p2l_dma_cyc_tbs   <= '0'; -- P2L dma WB cycle for bus arbitration
+		
+		
+		wait for period;
+		step <= 4;
+		
+		
+		wait for period;
+		step <= 5;
+		
+		
+		wait for 20*period;
+		step <= 6;
+		
+		wait for period;
+		step <= 7;
+		l_wr_rdy_tbs  <= '0'; 
+		
+		wait for period;
+        step <= 8;
+		l_wr_rdy_tbs  <= '1';
+		
+		wait;
+		
+		
+	end process stimuli_p;
+	
+  -----------------------------------------------------------------------------
+  -- L2P DMA master
+  -----------------------------------------------------------------------------
+  dut1 : l2p_dma_master
+    port map
+    (
+      clk_i   => clk_tbs,
+      rst_n_i => rst_n_tbs,
+
+      dma_ctrl_target_addr_i => dma_ctrl_target_addr_tbs,
+      dma_ctrl_host_addr_h_i => dma_ctrl_host_addr_h_tbs,
+      dma_ctrl_host_addr_l_i => dma_ctrl_host_addr_l_tbs,
+      dma_ctrl_len_i         => dma_ctrl_len_tbs,
+      dma_ctrl_start_l2p_i   => dma_ctrl_start_l2p_tbs,
+      dma_ctrl_done_o        => dma_ctrl_done_s,
+      dma_ctrl_error_o       => dma_ctrl_error_s,
+      dma_ctrl_byte_swap_i   => dma_ctrl_byte_swap_tbs,
+      dma_ctrl_abort_i       => dma_ctrl_abort_tbs,
+
+      ldm_arb_tvalid_o  => ldm_arb_tvalid_s,
+      ldm_arb_tlast_o => ldm_arb_tlast_s,
+      ldm_arb_tdata_o   => ldm_arb_tdata_s,
+      ldm_arb_req_o    => ldm_arb_req_s,
+      arb_ldm_gnt_i    => arb_ldm_gnt_tbs,
+
+      l2p_edb_o  => l2p_edb_s,
+      l_wr_rdy_i => l_wr_rdy_tbs,
+      l2p_rdy_i  => l2p_rdy_tbs,
+      tx_error_i => tx_error_tbs,
+
+      l2p_dma_clk_i   => clk_tbs,
+      l2p_dma_adr_o   => l2p_dma_adr_s,
+      l2p_dma_dat_i   => l2p_dma_dat_s2m_s,
+      l2p_dma_dat_o   => l2p_dma_dat_m2s_s,
+      l2p_dma_sel_o   => l2p_dma_sel_s,
+      l2p_dma_cyc_o   => l2p_dma_cyc_s,
+      l2p_dma_stb_o   => l2p_dma_stb_s,
+      l2p_dma_we_o    => l2p_dma_we_s,
+      l2p_dma_ack_i   => l2p_dma_ack_s,
+      l2p_dma_stall_i => l2p_dma_stall_tbs,
+      p2l_dma_cyc_i   => p2l_dma_cyc_tbs
+    );
+	
+	dut2:bram_wbs
+	generic map (
+		ADDR_WIDTH => wb_address_width_c,
+		DATA_WIDTH => wb_data_width_c 
+	)
+	port map (
+		-- SYS CON
+		clk			=> clk_tbs,
+		rst			=> rst_tbs,
+		
+		-- Wishbone Slave in
+		wb_adr_i	=> l2p_dma_adr_s(wb_address_width_c - 1 downto 0),
+		wb_dat_i	=> l2p_dma_dat_m2s_s,
+		wb_we_i		=> l2p_dma_we_s,
+		wb_stb_i	=> l2p_dma_stb_s,
+		wb_cyc_i	=> l2p_dma_cyc_s,
+		wb_lock_i	=> l2p_dma_stb_s,
+		
+		-- Wishbone Slave out
+		wb_dat_o	=> l2p_dma_dat_s2m_s,
+		wb_ack_o	=> l2p_dma_ack_s
+	);
+	
+	
+end Behavioral;
---
+++ wb_master/top_bench.vhd
@@ -0,0 +1,250 @@
+library IEEE;
+USE IEEE.STD_LOGIC_1164.all;
+USE IEEE.NUMERIC_STD.all;
+use IEEE.std_logic_unsigned.all; 
+
+
+entity top_bench is
+    generic (
+		constant period : time := 100 ns;
+		constant axis_data_width_c : integer := 64;
+		constant axis_rx_tkeep_width_c : integer := 64/8;
+		constant axis_rx_tuser_width_c : integer := 22;
+		constant wb_address_width_c : integer := 15;
+		constant wb_data_width_c : integer := 32
+	);
+	--port ();
+end top_bench;
+
+architecture Behavioral of top_bench is
+		signal clk_tbs : STD_LOGIC;
+		signal rst_tbs : STD_LOGIC;
+		-- Slave AXI-Stream
+		signal s_axis_rx_tdata_tbs : STD_LOGIC_VECTOR (axis_data_width_c - 1 downto 0);
+		signal s_axis_rx_tkeep_tbs : STD_LOGIC_VECTOR (axis_rx_tkeep_width_c - 1 downto 0);
+		signal s_axis_rx_tlast_tbs : STD_LOGIC;
+		signal s_axis_rx_ready_s : STD_LOGIC;
+		signal s_axis_rx_tuser_tbs : STD_LOGIC_VECTOR (axis_rx_tuser_width_c - 1 downto 0);
+		signal s_axis_rx_tvalid_tbs : STD_LOGIC;
+		-- Master AXI-Stream
+		signal m_axis_tx_tdata_s : STD_LOGIC_VECTOR (axis_data_width_c - 1 downto 0);
+		signal m_axis_tx_tkeep_s : STD_LOGIC_VECTOR (axis_data_width_c/8 - 1 downto 0);
+		signal m_axis_tx_tuser_s : STD_LOGIC_VECTOR (3 downto 0);
+		signal m_axis_tx_tlast_s : STD_LOGIC;
+		signal m_axis_tx_tvalid_s : STD_LOGIC;
+		signal m_axis_tx_ready_tbs : STD_LOGIC;
+		-- Wishbone Master
+		signal wb_adr_s : STD_LOGIC_VECTOR (wb_address_width_c - 1 downto 0);
+		signal wb_dat_o_s : STD_LOGIC_VECTOR (wb_data_width_c - 1 downto 0);
+		signal wb_dat_i_s : STD_LOGIC_VECTOR (wb_data_width_c - 1 downto 0);
+		signal wb_cyc_s : STD_LOGIC;
+		signal wb_sel_s : STD_LOGIC_VECTOR (wb_data_width_c - 1 downto 0);
+		signal wb_stb_s : STD_LOGIC;
+		signal wb_we_s : STD_LOGIC;
+		signal wb_ack_s : STD_LOGIC;
+		-- Test bench specific signals
+		signal step : integer range 1 to 10;
+		
+		Component axis_rx is
+		Port (
+			clk_i : in STD_LOGIC;
+			rst_i : in STD_LOGIC;
+			-- Slave AXI-Stream
+			s_axis_rx_tdata_i : in STD_LOGIC_VECTOR (axis_data_width_c - 1 downto 0);
+			s_axis_rx_tkeep_i : in STD_LOGIC_VECTOR (axis_data_width_c/8 - 1 downto 0);
+			s_axis_rx_tuser_i : in STD_LOGIC_VECTOR (21 downto 0);
+			s_axis_rx_tlast_i : in STD_LOGIC;
+			s_axis_rx_tvalid_i : in STD_LOGIC;
+			s_axis_rx_ready_o : out STD_LOGIC;
+			-- Master AXI-Stream
+			m_axis_tx_tdata_o : out STD_LOGIC_VECTOR (axis_data_width_c - 1 downto 0);
+			m_axis_tx_tkeep_o : out STD_LOGIC_VECTOR (axis_data_width_c/8 - 1 downto 0);
+			m_axis_tx_tuser_o : out STD_LOGIC_VECTOR (3 downto 0);
+			m_axis_tx_tlast_o : out STD_LOGIC;
+			m_axis_tx_tvalid_o : out STD_LOGIC;
+			m_axis_tx_ready_i : in STD_LOGIC;
+			-- Wishbone master
+			wb_adr_o : out STD_LOGIC_VECTOR (wb_address_width_c - 1 downto 0);
+			wb_dat_o : out STD_LOGIC_VECTOR (wb_data_width_c - 1 downto 0);
+			wb_dat_i : in STD_LOGIC_VECTOR (wb_data_width_c - 1 downto 0);
+			wb_cyc_o : out STD_LOGIC;
+			--wb_sel_o : out STD_LOGIC_VECTOR (wb_data_width_c - 1 downto 0);
+			wb_stb_o : out STD_LOGIC;
+			wb_we_o : out STD_LOGIC;
+			wb_ack_i : in STD_LOGIC
+		);
+		end component;
+		
+		component bram_wbs is
+		generic (
+			constant ADDR_WIDTH : integer := 16;
+			constant DATA_WIDTH : integer := 32 
+		);
+		port (
+			-- SYS CON
+			clk			: in std_logic;
+			rst			: in std_logic;
+			
+			-- Wishbone Slave in
+			wb_adr_i			: in std_logic_vector(wb_address_width_c-1 downto 0);
+			wb_dat_i			: in std_logic_vector(wb_data_width_c-1 downto 0);
+			wb_we_i			: in std_logic;
+			wb_stb_i			: in std_logic;
+			wb_cyc_i			: in std_logic; 
+			wb_lock_i		: in std_logic; -- nyi
+			
+			-- Wishbone Slave out
+			wb_dat_o			: out std_logic_vector(wb_data_width_c-1 downto 0);
+			wb_ack_o			: out std_logic		
+		);
+		end component;
+begin
+
+	clk_p: process
+	begin
+		clk_tbs <= '1';
+		wait for period/2;
+		clk_tbs <= '0';
+		wait for period/2;
+	end process clk_p;
+	
+	reset_p: process
+	begin
+	   rst_tbs <= '1';
+	   wait for period;
+	   rst_tbs <= '0';
+	   wait;
+	end process reset_p;
+	
+	stimuli_p: process
+	begin
+		step <= 1;
+		s_axis_rx_tdata_tbs <= (others => '0');
+		s_axis_rx_tkeep_tbs <= (others => '0');
+		s_axis_rx_tlast_tbs <= '0';
+		s_axis_rx_tuser_tbs <= (others => '0');
+		s_axis_rx_tvalid_tbs <= '0';
+		m_axis_tx_ready_tbs <= '1';
+		--wb_ack_s <= '0';
+		wait for period;
+		
+		wait for period;
+		step <= 2;
+		s_axis_rx_tdata_tbs <= X"0000000f40000001";
+		s_axis_rx_tkeep_tbs <= X"FF";
+		s_axis_rx_tlast_tbs <= '0';
+		s_axis_rx_tuser_tbs <= "11" & X"e4004";
+		s_axis_rx_tvalid_tbs <= '1';
+		m_axis_tx_ready_tbs <= '1';
+		--wb_ack_s <= '0';
+		wait for period;
+		step <= 3;
+		s_axis_rx_tdata_tbs <= X"a5a5a5a5f7d08000";
+		s_axis_rx_tkeep_tbs <= X"0F";
+		s_axis_rx_tlast_tbs <= '1';
+		s_axis_rx_tuser_tbs <= "10" & X"e4004";
+		s_axis_rx_tvalid_tbs <= '1';
+		m_axis_tx_ready_tbs <= '1';
+		--wb_ack_s <= '0';
+		wait for period;
+		step <= 4;
+		s_axis_rx_tdata_tbs <= X"0000000000000001";
+		s_axis_rx_tkeep_tbs <= X"FF";
+		s_axis_rx_tlast_tbs <= '0';
+		s_axis_rx_tuser_tbs <= "11" & X"60000";
+		s_axis_rx_tvalid_tbs <= '0';
+		m_axis_tx_ready_tbs <= '1';
+		--wb_ack_s <= '0';
+		wait for period;
+		step <= 5;
+		s_axis_rx_tdata_tbs <= X"0000000000000001";
+		s_axis_rx_tkeep_tbs <= X"FF";
+		s_axis_rx_tlast_tbs <= '0';
+		s_axis_rx_tuser_tbs <= "11" & X"60000";
+		s_axis_rx_tvalid_tbs <= '0';
+		m_axis_tx_ready_tbs <= '1';
+		--wb_ack_s <= '1';
+		wait for period;
+		wait for period;
+		wait for period;
+		step <= 6;
+		--wait until s_axis_rx_ready_o = '1';
+		s_axis_rx_tdata_tbs <= X"0000000f00000001";
+		s_axis_rx_tkeep_tbs <= X"FF";
+		s_axis_rx_tlast_tbs <= '0';
+		s_axis_rx_tuser_tbs <= "00" & X"e4004";
+		s_axis_rx_tvalid_tbs <= '1';
+		m_axis_tx_ready_tbs <= '1';
+		wait for period;
+		step <= 7;
+		--wait until s_axis_rx_ready_o = '1';
+		s_axis_rx_tdata_tbs <= X"592eaa50f7d08000";
+		s_axis_rx_tkeep_tbs <= X"FF";
+		s_axis_rx_tlast_tbs <= '1';
+		s_axis_rx_tuser_tbs <= "11" & X"60004";
+		s_axis_rx_tvalid_tbs <= '1';
+		m_axis_tx_ready_tbs <= '1';
+		wait for period;
+		wait for period;
+		wait for period;
+		wait for period;
+		wait for period;
+		wait for period;
+		
+		
+	end process stimuli_p;
+	
+	dut1:axis_rx
+	port map(
+		clk_i => clk_tbs,
+		rst_i => rst_tbs,
+		-- Slave AXI-Stream
+		s_axis_rx_tdata_i => s_axis_rx_tdata_tbs,
+		s_axis_rx_tkeep_i => s_axis_rx_tkeep_tbs,
+		s_axis_rx_tlast_i => s_axis_rx_tlast_tbs,
+		s_axis_rx_ready_o => s_axis_rx_ready_s,
+		s_axis_rx_tuser_i => s_axis_rx_tuser_tbs,
+		s_axis_rx_tvalid_i => s_axis_rx_tvalid_tbs,
+		-- Master AXI-Stream
+		m_axis_tx_tdata_o => m_axis_tx_tdata_s,
+		m_axis_tx_tkeep_o => m_axis_tx_tkeep_s,
+		m_axis_tx_tuser_o => m_axis_tx_tuser_s,
+		m_axis_tx_tlast_o => m_axis_tx_tlast_s,
+		m_axis_tx_tvalid_o => m_axis_tx_tvalid_s,
+		m_axis_tx_ready_i => m_axis_tx_ready_tbs,
+		-- Wishbone Master
+		wb_adr_o => wb_adr_s,
+		wb_dat_o => wb_dat_o_s,
+		wb_dat_i => wb_dat_i_s,
+		wb_cyc_o => wb_cyc_s,
+		--wb_sel_o => wb_sel_s,
+		wb_stb_o => wb_stb_s,
+		wb_we_o => wb_we_s,
+		wb_ack_i => wb_ack_s
+	);
+	
+	dut2:bram_wbs
+	generic map (
+		ADDR_WIDTH => wb_address_width_c,
+		DATA_WIDTH => wb_data_width_c 
+	)
+	port map (
+		-- SYS CON
+		clk			=> clk_tbs,
+		rst			=> rst_tbs,
+		
+		-- Wishbone Slave in
+		wb_adr_i	=> wb_adr_s,
+		wb_dat_i	=> wb_dat_o_s,
+		wb_we_i		=> wb_we_s,
+		wb_stb_i	=> wb_stb_s,
+		wb_cyc_i	=> wb_cyc_s,
+		wb_lock_i	=> wb_stb_s,
+		
+		-- Wishbone Slave out
+		wb_dat_o	=> wb_dat_i_s,
+		wb_ack_o	=> wb_ack_s
+	);
+	
+	
+end Behavioral;
